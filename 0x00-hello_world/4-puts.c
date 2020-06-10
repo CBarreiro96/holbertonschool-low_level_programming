27cc27b8a0e351b82da09f0b815ccf3bd7734cf0
Camilo Barreiro <1845@holbertonschool.com>
2020-06-10 20:01:02 +0000

    Púts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
  * Return: 0
  */
 int main(void)
-{  puts("Programming is like building a multilingual puzzle\n");
-    return (0);´
+{  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
