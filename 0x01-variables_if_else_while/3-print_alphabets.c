4da3a54e9ed3e8125fac3e34cbfb2b5f481a1672
Camilo Barreiro <1845@holbertonschool.com>
2020-06-11 22:09:31 +0000

    Imprimir el alfabeto

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * use of putchar
+ * Return: 0
+ */
+int main(void)
+{
+char b = 'a';
+while (b <= 'z')
+{putchar(b);
+b++;
+}
+b = 'A';
+while (b <= 'Z')
+{ putchar(b);
+b++; }
+putchar('\n');
+return (0);
+}
