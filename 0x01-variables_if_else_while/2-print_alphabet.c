a99bf6f3ffe7e99f0066985d6b69d0599af8f6b4
Camilo Barreiro <1845@holbertonschool.com>
2020-06-11 21:40:07 +0000

    Alfabeto

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - main block
+ * use of putchar
+ * Return: 0
+ */
+int main(void)
 {
- 
-  char b= 'a';
-  
-  while(b<='z')
-    {
-      putchar(b) ;
-      b++;
-    }
-  putchar('\n');
-  return 0;
+char b = 'a';
+
+while (b <= 'z')
+{ putchar(b);
+b++; }
+putchar('\n');
+return (0);
 }
