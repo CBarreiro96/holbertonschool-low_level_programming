769a7680a115916491f16e322b61fb5589ba69e1
Camilo Barreiro <1845@holbertonschool.com>
2020-06-11 23:12:31 +0000

    NUMERO

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-  int b;
-  for(b=0; b<=9 ; b++)
-    {  printf("%d",b);
-    }
-  putchar('\n');  
+int b;
+for (b = 0 ; b <= 9 ; b++)
+{ printf("%d", b);
+}
+putchar('\n');
 return (0);
 }
