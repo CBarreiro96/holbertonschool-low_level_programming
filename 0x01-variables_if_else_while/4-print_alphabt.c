b592e3bbe4960789b23ee08747a74891dea8eeb3
Camilo Barreiro <1845@holbertonschool.com>
2020-06-11 22:56:47 +0000

    Sin letra -q -e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - main block
- * use of putchar
+ * Description: Use `putchar` to print all letters but the letter 'q' and 'e'.
  * Return: 0
  */
 int main(void)
 {
-char b;
-while (b < 'z')
+char b = 'a';
+while (b <= 'z')
 {
 if (b != 'q' && b != 'e')
 putchar(b);
