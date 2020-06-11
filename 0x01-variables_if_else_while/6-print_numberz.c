ea858396eeaae83bf0abd2630f3327017f9648ca
Camilo Barreiro <1845@holbertonschool.com>
2020-06-11 23:32:58 +0000

    Imprimir numero con putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,14 +2,17 @@
 /**
  * main - main block
  * Description: Print all numbers of base 10, starting from 0.
+ * You are not allowed to use any variable of type char.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` twice.
  * Return: 0
  */
 int main(void)
 {
-  int b;
-  for (b = 0 ; b <= 9 ; b++)
-    { putchar(b + '0');
-    }
-  putchar('\n');
-  return (0);
+int b;
+for (b = 0 ; b <= 9 ; b++)
+{  putchar(b + '0');
+}
+putchar('\n');
+return (0);
 }
