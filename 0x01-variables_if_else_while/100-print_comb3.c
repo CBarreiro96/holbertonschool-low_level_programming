c0913e1af6610d90ffe7cb5dc3d560c1e47ac4ce
Camilo Barreiro <1845@holbertonschool.com>
2020-06-12 22:29:10 +0000

    Restricciones

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 /**
- *
- * 
-*/
+*
+*
+ */
 int main(void)
-{ int x, a, b;   
-  x=0;
-while(x < 100)
-{ 
-  a =x % 10;
-  b = x / 10;
-  if(b < a)
-    {
-  putchar(b + '0');
-  putchar(a +'0');
-  if(x < 89)
-    {
-  putchar(',');
-    putchar(' ');
-    }
-    }
-  x++;
+{ int x, a, b;
+x = 0;
+while (x < 100)
+{
+a = x % 10;
+b = x / 10;
+if (b < a)
+{
+putchar(b + '0');
+putchar(a + '0');
+if (x < 89)
+{
+putchar(',');
+putchar(' ');
 }
- putchar('\n');
-return(0);
+}
+x++;
+}
+putchar('\n');
+return (0);
 }
