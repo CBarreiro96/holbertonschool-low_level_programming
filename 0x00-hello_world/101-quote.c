02a9a06b5c41124f3c345c45c6632f2df621c09c
Camilo Barreiro <1845@holbertonschool.com>
2020-06-14 22:18:09 +0000

    Use write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
- * main - main block
- * Return: 1
+ * main - This program prints exactly the sentence of Dora Korpar,
+ * followed by a new line, to the standard error.
+ *
+ * Return: (1) to error.
  */
+
 int main(void)
 {
-int s = strlen("and that piece of art is useful\" - Dora Korpar,2015-10-19\n");
-write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n",
-s);
-putchar('\n');
-return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
 }
