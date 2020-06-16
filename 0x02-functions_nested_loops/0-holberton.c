810d67778738872d62b68e72623d86eb94fd970b
Camilo Barreiro <1845@holbertonschool.com>
2020-06-16 11:14:07 +0000

    Funcition

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - Print "Holberton" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	char Inicio[] = "Holberton";
+	int i = 0;
+	while (Inicio[i] != '\0')
+	{
+		_putchar(Inicio[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
