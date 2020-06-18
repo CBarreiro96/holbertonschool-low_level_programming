9f9f83fe17c25165a37802a912d637bda6ceaa69
Camilo Barreiro <1845@holbertonschool.com>
2020-06-18 14:04:46 +0000

    Function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *_isupper - Identify variable
+ *@c: int type number
+ *Return: identify 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c > 40 && c < 91)
+		return (1);
+	else
+		return (0);
+
+}
