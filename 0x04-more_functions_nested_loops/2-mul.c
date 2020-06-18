84ffa6a997163912868374771eba81d2d37bc5dc
Camilo Barreiro <1845@holbertonschool.com>
2020-06-18 14:55:19 +0000

    multiplication with two number with function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *mul - multiplication of two numbers
+ *@a: number type of integer
+ *@b: number type of integer
+ *Return: Return multiplies two integers a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
