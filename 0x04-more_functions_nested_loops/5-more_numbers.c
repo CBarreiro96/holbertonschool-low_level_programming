61854974248ae3302e8b5d6786bec5b73285775e
Camilo Barreiro <1845@holbertonschool.com>
2020-06-18 17:16:38 +0000

    Repeat number

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
 		j = 0;
 		while (j < 15)
 		{
-			if (j < 10)
+			if (j > 10)
 				_putchar('0' + j / 10);
-			_putchar('0' + j % 10);
+	     		_putchar('0' + j % 10);
 
 			j++;
 		}
