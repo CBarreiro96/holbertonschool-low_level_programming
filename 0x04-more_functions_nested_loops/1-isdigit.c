e1bfb74de5969d38107bbbd86c7589aa1c247293
Camilo Barreiro <1845@holbertonschool.com>
2020-06-18 14:32:41 +0000

    Function with digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
 /**
- *
- *
+ *_isdigit - Identify Number
+ *@c: variable type of integer
+ *Return:Return 1 or 0 depend of case
  */
 int _isdigit(int c)
 {
-	if(c > 47 && c < 58)
+	if (c > 47 && c < 58)
 		return (1);
 	else
 		return (0);
