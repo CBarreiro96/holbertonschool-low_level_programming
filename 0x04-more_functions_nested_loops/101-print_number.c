ec8052a643853348dfe2b74483ca2b861f614631
Camilo Barreiro <1845@holbertonschool.com>
2020-06-21 23:28:44 +0000

    Advance Number integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *print_number - Printf number
+ *@n: Number of type of integer
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	if ((n / 10) != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
