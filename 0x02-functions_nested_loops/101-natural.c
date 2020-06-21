a8138a312419229a25b3e14d47bb65d05e51a92e
Camilo Barreiro <1845@holbertonschool.com>
2020-06-21 19:47:24 +0000

    Natural number

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - printf sum of multiples of 3 or 5 until 1024
+ *Return: There are not anything.
+ */
+int main(void)
+{
+	int c;
+	int sum = 0;
+
+	for (c = 0; c < 1024; c++)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum += c;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
