2e4e0a04de513c3870793e5ad375071c82920351
Camilo Barreiro <1845@holbertonschool.com>
2020-06-23 15:29:02 +0000

    Fith exercise

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *rev_string - Revert the word of string
+ *@s:Pointer type char.
+ */
+void rev_string(char *s)
+{
+	int i;
+	int c;
+	int Long;
+	char k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+
+	Long = i;
+
+	for (i--, c = 0 ; c <= Long / 2 ; i--, c++)
+	{
+		k = s[c];
+		s[c] = s[i];
+		s[i] = k;
+	}
+}
