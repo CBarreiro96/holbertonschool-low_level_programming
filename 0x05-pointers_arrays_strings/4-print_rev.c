f82caa9a7fd9f1fa93e3dd11ec61fb53e2b5c331
Camilo Barreiro <1845@holbertonschool.com>
2020-06-23 12:44:13 +0000

    Printf reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *print_rev - Printf reverse of variable string
+ *@s: Pointer type of char.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	for (i--; i >= 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n')
+}
