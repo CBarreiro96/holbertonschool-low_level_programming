5beee7922533c9996fb0ecb5608f3a6116d08f40
Camilo Barreiro <1845@holbertonschool.com>
2020-06-23 16:31:17 +0000

    printf half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *puts_half - printf half of string
+ *@str: pointer tyṕe of char.
+ */
+void puts_half(char *str)
+{
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		;
+	}
+
+	for (i = i / 2; str[i] != 0 ; i++)
+	{
+		putchar(str[i]);
+	}
+}
