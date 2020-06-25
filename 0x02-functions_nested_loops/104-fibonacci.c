1c0b3daffbc9dfd8c75be879a7f6ea39d821251c
Camilo Barreiro <1845@holbertonschool.com>
2020-06-25 17:06:08 +0000

    prueba

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
+ * Return: 0
+ */
+int main(void)
+{
+	int C = 2;
+	long int i = 1, j = 2;
+	long int Suplent;
+
+	printf("%lu", i);
+        while (C <= 98)
+        {
+                if (C == 98)
+                {
+                        printf("%lu\n", j);
+                }
+                else
+                {
+                        printf("%lu, ", j);
+                }
+
+                Suplent = j;
+                j += i;
+                i = Suplent;
+                C++;
+        }
+
+        return (0);
+}
