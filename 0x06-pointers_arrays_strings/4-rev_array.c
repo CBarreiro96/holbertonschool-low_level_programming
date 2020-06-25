07b455a7902ad4486d91b1f6320d45b85cb5cffc
Camilo Barreiro <1845@holbertonschool.com>
2020-06-25 22:21:39 +0000

    Revert array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *reverse_array - Invert array
+ *@a:Pointer type of integer
+ *@n:variable type of integer
+ */
+void reverse_array(int *a, int n)
+{
+	int h;
+	int i;
+
+	for (i = 0, n--; i < n / 2 ; n--, i++)
+	{
+		h = a[i];
+		a[i] = a[n];
+		a[n] = h;
+	}
+}
