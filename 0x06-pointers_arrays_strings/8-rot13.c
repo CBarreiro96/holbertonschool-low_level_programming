e4d60b8ed3495ce7cea126ca88b05122e05870f8
Camilo Barreiro <1845@holbertonschool.com>
2020-06-26 00:43:03 +0000

    Last exercise

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *rot13 - function that encodes a string using rot13.
+ *@s: Pointer type of char
+ *Return: return s
+ */
+
+
+char *rot13(char *s)
+{
+	int i;
+	char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char storel[] = "nopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+		{
+			s[i] = (s[i] - 65 > 25) ?
+				storel[s[i] - 97] : storeh[s[i] - 65];
+		}
+	}
+	return (s)
+}
