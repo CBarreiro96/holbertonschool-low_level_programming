35c18310d7cd3da4ab3386f9386a127a91987cf9
Camilo Barreiro <1845@holbertonschool.com>
2020-06-28 20:49:02 +0000

    prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *print_chessboard - function that prints the chessboard
+ *@a: Multi-dimensional Arrays
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	i = 0;
+
+	while(i < 8)
+	{
+		j = 0;
+		while(j < 8)
+		{
+			putchar(a[i][j]);
+			j++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
