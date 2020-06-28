00696fd5e78783eabfb9696eaffe54546af46735
Camilo Barreiro <1845@holbertonschool.com>
2020-06-28 19:10:33 +0000

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *_strstr - function that locates a substring.
+ *@haystack: Pointer type of char.
+ *@needle: Pointer type of char.
+ *Return: return pointer.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+	while (*needle)
+	{
+		while (*haystack)
+		{
+			if (*haystack == *needle)
+			{
+				return (haystack);
+			}
+			haystack++;
+		}
+
+		needle++;
+	}
+	return (0);
+}
