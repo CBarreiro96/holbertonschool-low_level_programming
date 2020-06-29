6c1fb5e86576db99d26b70513b60b699093de01f
Camilo Barreiro <1845@holbertonschool.com>
2020-06-29 00:53:44 +0000

    pointer to pointer

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ *set_string - function that sets the value of a pointer to a char.
+ *@s: Pounter to pounter type iof char
+ *@to: Pointer type of char
+ */
+void set_string(char **s, char *to)
+{
+	*s=to;
+}
