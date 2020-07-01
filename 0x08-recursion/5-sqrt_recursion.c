7002325ac51944d770960518c1cbf7ed692a828e
Camilo Barreiro <1845@holbertonschool.com>
2020-07-01 22:07:09 +0000

    sqrt with two function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,16 @@
 #include "holberton.h"
 /**
+ *_sqrt_recursion -function that returns the natural square root of a number.
+ *@n: Variable type of integer
+ *Return: return function about natural square.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (Procces(n, 1));
+}
+
+/**
  *Procces - Procces about sqrt
  *@a: Variable type integer  and equal and n.
  *@i: Variable type of integer and conditional.
@@ -9,20 +20,10 @@ int Procces(int a, int i)
 {
 	int Pow = i * i;
 
-	if (Pow == a)
-		return (i);
+        if (Pow == a)
+                return (i);
 	else if (Pow < a)
-		Procces(a, i + 1);
-	else if (Pow > a)
-		return (-1);
-}
-/**
- *_sqrt_recursion -function that returns the natural square root of a number.
- *@n: Variable type of integer
- *Return: return function about natural square.
- */
-
-int _sqrt_recursion(int n)
-{
-	return (Procces(n, 1));
+                Procces(a, i + 1);
+        else if (Pow > a)
+                return (-1);
 }
