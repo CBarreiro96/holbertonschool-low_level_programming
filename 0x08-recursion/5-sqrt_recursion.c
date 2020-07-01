cdd9965de238bde0cabfccfa477961efccb85caf
Camilo Barreiro <1845@holbertonschool.com>
2020-07-01 22:25:12 +0000

    sqrt with two function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,10 +22,10 @@ int Procces(int a, int i)
 {
 	int Pow = i * i;
 
-	if (Pow == a)
-		return (i);
-	else if (Pow < a)
-		Procces(a, i + 1);
-	else if (Pow > a)
+	if(Pow == a)
+		return(i);
+	else if(Pow < a)
+		return Procces(a, i + 1);
+	else
 		return (-1);
 }
