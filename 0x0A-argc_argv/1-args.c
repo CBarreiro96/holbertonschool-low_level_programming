bb395c1be21fa0a3386fb86afdf9b58554b1795a
Camilo Barreiro <1845@holbertonschool.com>
2020-07-05 01:43:18 +0000

    Count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - program that prints the number of arguments passed into it.
+ *@argc:Argument count
+ *@argv: Argument vector, array of string.
+ */
+int main(int argc, char* argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
