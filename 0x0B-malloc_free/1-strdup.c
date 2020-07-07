5377de886fc6dd7a2cadeb57e07bc7f03ce87f71
Camilo Barreiro <1845@holbertonschool.com>
2020-07-07 19:35:03 +0000

    Malloc 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_strdup - contains a copy of the string given as a parameter.
+ *@str: Pointer
+ *Return. return depend of case.
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	int i, j;
+
+	if (str == NULL)
+	{
+		return NULL;
+	}
+	for (i = 0; str[i] != '\0';i++)
+	{
+		;
+	}
+
+	a = malloc(i*sizeof(char));
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+	{
+		a[j] = str[j];
+	}
+	return (a);
+}
