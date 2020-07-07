ceed4366787f662ce75c941e865dfdd3149c81d9
Camilo Barreiro <1845@holbertonschool.com>
2020-07-07 21:08:50 +0000

    concatenate string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,15 +10,18 @@ char *str_concat(char *s1, char *s2)
 {
 	char *a;
 	int i, j, n, c;
-	for(i = 0; s1 [i] != '\0' ; i++)
+
+	for (i = 0; s1[i] != '\0' ; i++)
 		;
-	for(j = 0; s2[j] != '\0'; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 		;
-	n= i + j;
-	a = malloc(n * sizeof(*a));
-	for(c = 0; c <= i; c++)
+	n = i + j;
+	a = malloc(n * sizeof(*a) + 1);
+
+	for (c = 0; c <= i; c++)
 		a[c] = s1[c];
-	for(c = 0; c<= j ; c++, i++)
+	for (c = 0; c <= j ; c++, i++)
 		a[i] = s2[c];
+	a[n] = '\0';
 	return (a);
 }
