63777879733465c7e1cb20ce9bde19f2aade1b08
Camilo Barreiro <1845@holbertonschool.com>
2020-07-08 10:42:11 +0000

    Malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,25 +11,31 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int i, j;
+	int **x;
+	int i;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-	grid = malloc(height * sizeof(*grid));
+	/* Allow dinamyc memory */
+	x = (int **)calloc(width, sizeof(int *));
+	if (x == NULL)
+		return (NULL);
 
-	for (i = 0; i < height; i++)
+	for (i = 0; i < width; i++)
 	{
-		grid[i] = malloc(width * sizeof(**grid));
-		if (grid[i] == NULL)
+		x[i] = (int *)calloc(height, sizeof(int));
+
+		if (x[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
-				free(grid[i]);
-			free(grid);
+			{
+				free(x[i]);
+			}
+			free(x[i]);
 			return (NULL);
 		}
-		for (j = 0; j < width; j++)
-			grid[i][j] = 0;
 	}
+	return (x);
 
-	return (grid);
 }
