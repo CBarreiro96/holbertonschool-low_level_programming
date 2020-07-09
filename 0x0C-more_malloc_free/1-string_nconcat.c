cd60b5c2575a4bef41d049dde2e13787a3e8dc4a
Camilo Barreiro <1845@holbertonschool.com>
2020-07-09 22:11:40 +0000

    Concatenate string with malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *string_nconcat - function that concatenates two strings.
+ *@s1: Pointer type of char
+ *@s2: Pointer type of char
+ *@n: Variable type of unsigned integer.
+ *Return: return pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *a;
+	unsigned int i, j, k, l;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] ; i++)
+		;
+	for (l = 0; s2[l]; l++)
+		;
+
+	a = (char *)malloc((i + n + 1) * sizeof(char));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (j = 0, k = 0; j < (n + i); j++)
+	{
+		if (j < i)
+			a[j] = s1[j];
+		else if (j <= (l + i))
+			a[j] = s2[k++];
+		else
+		{
+			a = (char *)realloc(a, i + l + 1);
+			break;
+		}
+	}
+	if (n < l)
+		a[j] = '\0';
+
+	return (a);
+
+}
