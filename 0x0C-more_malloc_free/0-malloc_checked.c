c1c41b1d9c7feec1f22cb5427311dc5e27714d63
Camilo Barreiro <1845@holbertonschool.com>
2020-07-09 17:16:34 +0000

    Use Generic Pointer

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
 #include <stdlib.h>
-
+/**
+ *malloc_checked - function that allocates memory using malloc.
+ *@b: Variable type of unsigned integer.
+ *Return: This return pointer.
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *str;
 
 	/* Initial memory allocation */
-	str = (char*) malloc(b);
+	str = (char *) malloc(b);
 	if (str == NULL)
-		exit (98);
+		exit(98);
 
 	return (str);
 }
