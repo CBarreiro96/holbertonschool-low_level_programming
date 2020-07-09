77b9b21e3f64e815a29d234cff44709f887cc0f4
Camilo Barreiro <1845@holbertonschool.com>
2020-07-09 22:57:47 +0000

    use calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	a=(char *) calloc(nmemb,size);
+	a = (char *)calloc(nmemb, size);
 	if (a == NULL)
 		return (NULL);
 	return (a);
