ce25884912c9276efb40a53061e8067e44e05109
Camilo Barreiro <1845@holbertonschool.com>
2020-07-09 22:55:49 +0000

    use calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *_calloc - function that allocates memory for an array, using malloc.
+ *@nmemb: Variable type of unsigned integer
+ *@size: Variable typr of integer
+ *Return: return pointer.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a=(char *) calloc(nmemb,size);
+	if (a == NULL)
+		return (NULL);
+	return (a);
+}
