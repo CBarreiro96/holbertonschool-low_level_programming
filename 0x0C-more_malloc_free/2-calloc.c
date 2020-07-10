bfe1c9919462206538afb10ca02c6972c56feb89
Camilo Barreiro <1845@holbertonschool.com>
2020-07-10 00:30:37 +0000

    What is calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,17 +8,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
+	int *a;
 	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb * size);
-	if (p == NULL)
+	a = malloc(nmemb * size);
+	if (a == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
+	for (i = 0; i < (nmemb * size); i++)
+		a[i] = 0;
 
-	return (p);
+	return (a);
 }
