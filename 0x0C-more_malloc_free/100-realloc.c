040cdebc30fd6d6f17db47be7e718a29c7f13aab
Camilo Barreiro <1845@holbertonschool.com>
2020-07-12 18:51:51 +0000

    funtion realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+  *_realloc - reallocates a memory block using malloc and free
+  *@ptr: Pointer typr of pointer.
+  *@old_size: Variable type unsigned
+  *@new_size: New size of malloc
+  *Return:return the varible.
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *a;
+	char *copy;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		a = malloc(new_size);
+		if (a == NULL)
+			return (NULL);
+		return (a);
+	}
+
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	a = malloc(new_size);
+	copy = ptr;
+
+	for (i = 0; i < old_size; i++)
+		a[i] = copy[i];
+	free(ptr);
+	return (a);
+}
