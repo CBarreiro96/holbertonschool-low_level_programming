d58afa5c72d875ca1fcd184156150d3d83134a09
Camilo Barreiro <1845@holbertonschool.com>
2020-07-15 21:46:25 +0000

    function as argument

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+*array_iterator - Function given as a parameter on each element of an array.
+*@array: Pointer type of integer.
+*@size:is the size of the array type sixte_t
+*@action: function pointer that takes a int argument.
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
