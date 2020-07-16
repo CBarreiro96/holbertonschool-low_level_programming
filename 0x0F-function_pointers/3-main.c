48c070a41db883aba8434e2351e1edb3a9769356
Camilo Barreiro <1845@holbertonschool.com>
2020-07-16 01:33:19 +0000

    Exercise 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+*main - Start code.
+*@argc: argument count
+* @argv: arguments passed to main as an array of strings
+*Return: return result.
+*/
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	int (*c)(int, int);
+
+	if (argc != 5)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	c = get_op_func(argv[2]);
+	if (c == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", c(a, b));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+*op_add - Sum number
+*@a: Variable type integer
+*@b:Variable type integer
+*Return: return result.
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+*op_sub - Diference number
+*@a: Variable type integer
+*@b:Variable type integer
+*Return: return result.
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+*op_mul - multiplicer number
+*@a: Variable type integer
+*@b:Variable type integer
+*Return: return result.
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+*op_div - divide number
+*@a: Variable type integer
+*@b:Variable type integer
+*Return: return result.
+*/
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error");
+		return (100);
+	}
+
+	return (a / b);
+}
+
+/**
+*op_mod - mod number
+*@a: Variable type integer
+*@b:Variable type integer
+*Return: return result.
+*/
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error");
+		return (100);
+	}
+	return (a % b);
+}
