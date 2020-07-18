88a5a2fd3424ee318eb4239ee09e15225c1bf0ff
Camilo Barreiro <1845@holbertonschool.com>
2020-07-18 18:04:47 +0000

    Configuration betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,20 +10,15 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	unsigned int i, sum;
 
-/*Condition propouse exercise*/
 	if (n == 0)
 		return (0);
 
-/* Initialize the argument list. */
 	va_start(ap, n);
 
-/* Get the next argument value. */
 	sum = 0;
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
-/* Clean up. */
 	va_end(ap);
-/*Return result*/
 	return (sum);
 }
