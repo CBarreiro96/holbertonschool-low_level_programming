98bc5d518488034f65b45c943501d876c001780f
Camilo Barreiro <1845@holbertonschool.com>
2020-07-18 20:36:39 +0000

    Directoryt

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,10 +2,25 @@
 #define _VARIADIC_FUNCTION_H_
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct check
+{
+	char *type;
+	void (*funtion_printf_anything)();
+} check;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+void print_int(va_list list);
+void print_float(va_list list);
+void print_char(va_list list);
+void print_string(va_list list);
 #endif /*_VARIADIC_FUNCTION_H_*/
