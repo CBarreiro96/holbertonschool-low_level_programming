ff041daea02283b70d873c8594b4951e6f27b5d5
Camilo Barreiro <1845@holbertonschool.com>
2020-07-24 01:18:44 +0000

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,6 +7,32 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	rerturn (new_node);
+	ist_t *new_node;
+	list_t *current_node;
+	int i;
+	char  *strcpy;
+
+	current_node = *head;
+	while (current_node && current_node->next != NULL)
+		current_node = current_node->next;
+
+	strcpy = strdup(str);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->len = i;
+	new_node->str = strcpy;
+	new_node->next = NULL;
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	if (current_node)
+		current_node->next = new_node;
+	else
+		*head = new_node;
+	return (new_node);
 }
