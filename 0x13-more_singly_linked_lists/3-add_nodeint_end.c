249faa79be499ecb1177d5d6b13b9ae2cfc7399c
Camilo Barreiro <1845@holbertonschool.com>
2020-07-28 17:29:31 +0000

    Correction Betty

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,28 +1,28 @@
 #include "lists.h"
 /**
- *add_nodeint-adds a new node at the beginning of a listint_t list.
+ *add_nodeint_end-adds a new node at the end of a listint_t list.
  *@head:Double pointer to head node
  *@n:int value to store in new node
  *Return: Return new node or NULL if it is
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-        listint_t *new;
-        listint_t *current;
+	listint_t *new;
+	listint_t *current;
 
-        current = *head;
+	current = *head;
 /*Moves curret in diferent nodes*/
 	while (current && current->next != NULL)
 		current = current->next;
 
-        new = malloc(sizeof(listint_t));
-        if (new == NULL)
-                return (NULL);
-        new->n = n;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
 /*Conect diferent node*/
-        if (current)
-                current->next = new ;
-        else
-                *head = new;
-        return (new);
+	if (current)
+		current->next = new;
+	else
+		*head = new;
+	return (new);
 }
