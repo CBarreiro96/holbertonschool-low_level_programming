ae33f349ef40ff3fa69c330d93a546165542e611
Camilo Barreiro <1845@holbertonschool.com>
2020-09-05 04:06:51 +0000

    Exercise add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * add_dnodeint -
  *
@@ -7,5 +6,26 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *current, *new;
 
+	current = *head;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	if (current == NULL)
+		new->next = NULL;
+	else
+	{
+		while (current->prev)
+			current = current->prev;
+
+		current->prev = new;
+		new->next = current;
+	}
+	*head = new;
+
+	return (*head);
 }
