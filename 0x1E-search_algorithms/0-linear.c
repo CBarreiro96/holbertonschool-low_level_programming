dbdf28f793dc1f9b2ca8f8628ed63c70e330a1d7
Camilo Barreiro <1845@holbertonschool.com>
2021-01-13 21:43:35 +0000

    fix code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,11 @@
 #include "search_algos.h"
+
 /**
  *linear_search - Sequentially search an array for a value
  *@array: Pointer to first element in array to search
  *@size: Number of element in the array
  *@value: value to search for
- *Return: first index where value is located or -1 if
- *error or not found
+ *Return: first index where value is located or -1
 */
 int linear_search(int *array, size_t size, int value)
 {
