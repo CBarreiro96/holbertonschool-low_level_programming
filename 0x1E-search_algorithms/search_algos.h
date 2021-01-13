6137403fedbb1262b2d91f73405b35989b2e7c94
Camilo Barreiro <1845@holbertonschool.com>
2021-01-13 21:46:32 +0000

    fix header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 /*Auxiliar Prototype*/
-void print_array(int *array, size_t inc, size_t size);
+void print_array(int *array, int begin, int final);
 
 /*Prototypes*/
 int linear_search(int *array, size_t size, int value);
