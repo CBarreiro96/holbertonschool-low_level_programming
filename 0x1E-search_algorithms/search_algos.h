a4705bbcae58098c16d4b1ac4e51e76964344736
Camilo Barreiro <1845@holbertonschool.com>
2021-01-13 21:34:11 +0000

    header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,18 @@
+#ifndef SEARCH_ALGORITHM
+#define SEARCH_ALGORITHM
+
+#include <stdio.h>
+
+/*Auxiliar Prototype*/
+void print_array(int *array, size_t inc, size_t size);
+
+/*Prototypes*/
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+
+
+
+
+#endif /*SEARCH_ALGORITHM*/
