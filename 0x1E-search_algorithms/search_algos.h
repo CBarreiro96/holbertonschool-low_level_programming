ff8b45d463ffb75b57a4f92a0385ea92e2bd3bec
Camilo Barreiro <1845@holbertonschool.com>
2021-01-15 02:17:24 +0000

    Interpolation search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,6 +9,8 @@ void print_array(int *array, int begin, int final);
 /*Prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 
 
